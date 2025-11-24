7f439925e89c7b9d7ee1510e8a4044a2cd09bc7e
sm1le-devops <gvh423699@gmail.com>
2025-11-24 13:17:28 +0100

    delete bugs

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from telegram.ext import Application, MessageHandler, filters, CommandHandler
 import re
 from datetime import datetime, timedelta
 import os
+from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_NICK = "@conterbloxadmin"
 MODER_NICK = "@sm1le697"
-from dotenv import load_dotenv
+
 
 BAN_PHRASES = [
     r"переходите\s+в\s+мою\s+телеграм\s+группу",
